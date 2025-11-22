bdc9b8df48461f0b76f4a2ceacb7a2d5721754cf
Dhruvdesai793 <blixture793@gmail.com>
2025-11-23 02:16:14 +0530

    7th

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react(), tailwindcss()],
-    // Only apply the repo path in production build, use root for dev
-    base: mode === 'production' ? '/Kyu-Nahi-Ho-Rahi-Padhai/' : '/',
-  }
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), tailwindcss()],
+  base: mode === 'production'
+    ? '/Kyu-Nahi-Ho-Rahi-Padhai/'   
+    : '/'
+}))
